098a631b49aa41b4a4347b00bde7b470d0f0542f
Scott <scott.mclaughlin01@gmail.com>
2023-01-09 15:54:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import img from './media/default_person.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <body>
+        <h1>Reddit Mini</h1>
+        <nav>
+          <input type="text" placeholder='Search' ></input>
+        </nav>
+        <header>
+          <button className='sub'>
+            <img src={img} alt="logo 1"/>
+            <div id="text">text</div>
+          </button>
+          <button className='sub'>
+            <img src={img} alt="logo 2"/>
+            <div id="text">text</div>
+          </button>
+          <button className='sub'>
+            <img src={img} alt="logo 3"/>
+            <div id="text">text</div>
+          </button>
+        </header>
+        <main>
+          <div>POST 1</div>
+          <div>POST 2</div>
+           
+        </main>
+        <footer>
+
+        </footer>
+      </body>
     </div>
   );
 }
